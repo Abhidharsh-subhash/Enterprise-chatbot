ba850ee9109ec122506f3ab99fc5f6b29eead649
Abhidharsh s s <abhidharsh@futuregeninnovationlab.com>
2025-10-14 18:07:50 +0530

    redis configuration has also been completed

--- app/core/events.py
+++ app/core/events.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.db.postgres.database import engine
+from app.db.redis.redis import redis_client
 from sqlalchemy import text
 import asyncio
 
@@ -14,7 +15,16 @@ async def lifespan(app: FastAPI):
     except Exception as e:
         print("❌ PostgreSQL connection failed:", e)
 
+    try:
+        pong = await redis_client.ping()
+        print("✅ Redis connected:", pong)
+    except Exception as e:
+        print("❌ Redis connection failed:", e)
+
     yield
 
     await engine.dispose()
     print("🔌 PostgreSQL engine disposed")
+
+    await redis_client.aclose()
+    print("🔌 Redis connection closed")
--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,16 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.postgres.database import async_session
+from app.db.redis.redis import redis_client
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+
+
+async def get_redis():
+    try:
+        yield redis_client
+    finally:
+        pass
