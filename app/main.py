40ac2df06342befb7f008810dc6e65434dc08a27
Abhidharsh s s <abhidharsh@futuregeninnovationlab.com>
2025-11-07 20:55:07 +0530

    new logic for swagger

--- app/main.py
+++ app/main.py
@@ -13,7 +13,10 @@ async def root():
 
 
 @app.get("/docs", include_in_schema=False)
-async def get_swagger(request: Request):
+async def custom_swagger_ui_html(req: Request):
+    root_path = req.scope.get("root_path", "").rstrip("/")
+    openapi_url = root_path + app.openapi_url
     return get_swagger_ui_html(
-        openapi_url=request.scope.get("root_path") + "/openapi.json", title="Swagger"
+        openapi_url=openapi_url,
+        title="API",
     )
