690a4de5db2d98b109dc58191fb2b1f8eea9dc4c
Abhidharsh s s <abhidharsh@futuregeninnovationlab.com>
2025-11-07 20:50:27 +0530

    swagger implemented

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.core.events import lifespan
 from app.routers import api_router
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI(title="self-hosted chatbot", lifespan=lifespan)
 app.include_router(api_router)
@@ -9,3 +10,10 @@ app.include_router(api_router)
 @app.get("/test")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/docs", include_in_schema=False)
+async def get_swagger(request: Request):
+    return get_swagger_ui_html(
+        openapi_url=request.scope.get("root_path") + "/openapi.json", title="Swagger"
+    )
