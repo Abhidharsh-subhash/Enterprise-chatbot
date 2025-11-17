383786306cb4412e3653e7142547b98fcce0ba3f
Abhidharsh s s <abhidharsh@futuregeninnovationlab.com>
2025-11-17 12:37:38 +0530

    cors error issue resolved

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Request
 from app.core.events import lifespan
 from app.routers import api_router
-from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="self-hosted chatbot", lifespan=lifespan)
 app.include_router(api_router)
 
+# Allow all origins (any domain or IP)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- this allows any domain
+    allow_credentials=True,  # allow cookies/auth headers
+    allow_methods=["*"],  # allow GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # allow all headers
+)
 
 @app.get("/test")
 async def root():
