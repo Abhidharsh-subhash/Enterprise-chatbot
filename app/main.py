fe0f3de310ab1379868d7accc8392a430f052a98
Abhidharsh s s <abhidharsh@futuregeninnovationlab.com>
2025-12-09 14:25:45 +0530

    exception response format is changed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from app.core.events import lifespan
 from app.routers import api_router
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="self-hosted chatbot", lifespan=lifespan)
 app.include_router(api_router)
@@ -9,3 +10,14 @@ app.include_router(api_router)
 @app.get("/test")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "status_code": exc.status_code,
+            "message": exc.detail,  # rename 'detail' → 'message'
+        },
+    )
